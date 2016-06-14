8b3c960bfa1ea4bcf812dda75cfc349dcac5d4dd
Sergey Skovorodnikov <sg.skov@gmail.com>
2016-06-14 16:01:28 -0700

    Added code that adds HTML to the page. It is dirty for now, but working as needed.

--- blocker.js
+++ blocker.js
@@ -14,12 +14,39 @@ function printBlockedUrls() {
 }
 
 function checkUrlCallback(result) {
-    /* Used by checkUrl. Blocks loading of current page if it's blocked. */
+    /* Used by checkUrl. Blocks current page. */
     var blocked = result.blocked;
     for (i in blocked) {
         if (location.hostname === blocked[i]) {
             console.log("[WARNING] " + location.hostname
                 + " is blocked. Replacing page.");
+						// var overlay_div = document.createElement("div");
+						// overlay_div.innerHTML = "hello from me";
+						// var overlay_span = document.createElement("span");
+						// overlay_div.appendChild(overlay_span);
+						// // append to DOM
+						// document.body.appendChild(overlay_div);
+
+						//alert("blocked page");
+						//Creating Elements
+						// var el = document.getElementById("top_of_page");
+						// console.log("got element class " + el.parentElement.getAttribute("class"));
+
+						// var divNode = document.createElement("div")
+						// divNode.setAttribute("id", "sergey");
+						// divNode.innerHTML = "skov";
+						// console.log("divNode id " + divNode.getAttribute("id"));
+						// //Appending to DOM 
+						// el.appendChild(divNode);
+						// document.body.appendChild(divNode);
+
+						var newDiv = document.createElement("div");
+						newDiv.innerHTML = "hello from new div";
+						newDiv.style.width = "100px";
+						newDiv.style.height = "100px";
+						newDiv.style.background = "red";
+						newDiv.style.color = "white";
+						document.body.appendChild(newDiv);
         }
     }
 }
